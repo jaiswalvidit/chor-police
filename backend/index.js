51917c67a19a7265d168fc56609d137f88322da0
jaiswalvidit <jaiswalvidit23@gmail.com>
2023-12-27 21:28:51 +0530

    added dotenv

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from chor-police');
+});
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
